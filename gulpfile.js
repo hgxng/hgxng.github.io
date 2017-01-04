c483669db9c4612198078db3dd6e795062f52200
Aderinsola Akintilo <aderinsola419@gmail.com>
2017-01-04 23:20:07 +0100

    getting it up and running

--- gulpfile.js
+++ gulpfile.js
@@ -84,21 +84,21 @@ gulp.task("server", function () {
 /*
     #   
 */
-gulp.task("heroku", ["build"], function () {
-    // set the port of our application
-    // process.env.PORT lets the port be set by Heroku
-    var port = process.env.PORT || 8080;
-    // set the view engine to ejs
-    app.set('view engine', 'jst');
-    // make express look in the public directory for assets (css/js/img)
-    app.use(express.static(__dirname + '/build'));
-    // set the home page route
-    app.get('/', function (req, res) {
-        // ejs render automatically looks in the views folder
-        res.render('index');
-    });
-    //
-    app.listen(port, function () {
-        console.log('Our app is running on http://localhost:' + port);
-    });
-});
+//gulp.task("heroku", ["build"], function () {
+//    // set the port of our application
+//    // process.env.PORT lets the port be set by Heroku
+//    var port = process.env.PORT || 8080;
+//    // set the view engine to ejs
+//    app.set('view engine', 'jst');
+//    // make express look in the public directory for assets (css/js/img)
+//    app.use(express.static(__dirname + '/build'));
+//    // set the home page route
+//    app.get('/', function (req, res) {
+//        // ejs render automatically looks in the views folder
+//        res.render('index');
+//    });
+//    //
+//    app.listen(port, function () {
+//        console.log('Our app is running on http://localhost:' + port);
+//    });
+//});
