f7f3fbb500fb85e7b0ef6b71239133798849496a
Aderinsola Akintilo <aderinsola419@gmail.com>
2017-01-15 15:03:20 -0800

    got the animation to work

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,10 +2,20 @@ $(document).ready(function () {
   $(function () {
     $("#scrollButton a").on('click', function (e) {
       e.preventDefault();
-      console.log( "should animate you know")
-      $('html, body').animate({
-        scrollTop: $($(this).attr('href')).offset().top
-      }, 450, 'linear');
+      scrollPage(this);
+      animateNavbar();
     });
   });
 })
+
+function scrollPage(_this) {
+  $('html, body').animate({
+    scrollTop: $($(_this).attr('href')).offset().top
+  }, 450, 'linear');
+}
+
+function animateNavbar() {
+  $(".nav .element").css({
+    color: "black"
+  })
+}
